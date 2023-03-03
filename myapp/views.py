4b50aad41e79c8840b4dc5b6c1403f834cfa157d
pidokige02 <pidokige0204@gmail.com>
2023-03-04 06:37:08 +0900

    7/14 homepage design

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render, HttpResponse
 import random
 
 # Create your views here.
+topics = [  #list
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},  # dictionary
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
+
 def index(request):
-    return HttpResponse('<h1>Random</h1>'+str(random.random()))
+    global topics  # 전역변수임
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>' # f를 붙이면 중괄호일때 변수를 바로 사용할 수 있다
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
 
 def create(request):
     return HttpResponse('Create')
